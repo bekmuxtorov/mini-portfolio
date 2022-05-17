dbcb478f6a63bcca3911397cb02e4de65e7c425c
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2022-05-17 10:49:53 +0500

    added typing js

---
+++ apps/app.js
@@ -0,0 +1,17 @@
+var typed = new Typed('.an', {
+    strings: [
+        "ASADBEK MUXTOROV"
+    ],
+    typeSpeed: 100,
+    loop: false
+});
+
+var typed = new Typed('.an2', {
+    strings: [
+        "19 years old, from Fergana, student at the University, Front-End Engineer"
+    ],
+    typeSpeed: 100,
+    loop: false,
+    startDelay: 300,
+    showCursor: true
+});
